56db768a1624355d3c8c109eaf8146d49eeb6b91
clara <clasei@proton.me>
2023-11-10 11:01:54 +0100

    data.service added to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'galaxy-hitchhiker';
+  quotes: any[] = [];   // quotes' array
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.getQuotes().subscribe(data => {
+      this.quotes = data;
+    });
+  }
 }
